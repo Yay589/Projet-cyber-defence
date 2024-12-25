aa8af67c120932848f10ab16778b9756e4493bd9
ahmedsafta71 <ahmedsafta71@gmail.com>
2024-12-25 21:36:59 +0100

    ssh-container-with-docker-bridge-ip-address

--- docker/dvwa-with-ssh/setup_ssh.sh
+++ docker/dvwa-with-ssh/setup_ssh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Install OpenSSH server
-apt-get update && apt-get install -y openssh-server
+# Install OpenSSH server  |  Python Packages  should be installed to make the ansible work  properly 
+apt-get update && apt-get install -y openssh-server && apt install -y python3
 
 # Create directory for sshd to run
 mkdir /var/run/sshd
@@ -12,6 +12,8 @@ echo "root:root" | chpasswd
 # Enable root login and password authentication in sshd_config
 sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+# Open the ssh  port 22
+sed -i 's/^#Port 22/Port 22/' /etc/ssh/sshd_config
 
 # Start SSH service
 service ssh start
