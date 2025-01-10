57f848e4a63ff11bb65eba6e0012fb462315e037
Yay589 <yacine.said@insa-cvl.fr>
2025-01-10 22:30:06 +0100

    Revert "hey guysgit add ."
    This reverts commit 1533a58943d7c2523c159d839b011345235ea78d.

--- Client/dvwa-with-ssh/Start_Client.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "###DANS CLIENT###" && pwd && docker-compose -f ../Client/dvwa-with-ssh/docker-compose.yaml down && docker-compose -f ../Client/dvwa-with-ssh/docker-compose.yaml --env-file ./var.env build && docker-compose -f ../Client/dvwa-with-ssh/docker-compose.yaml --env-file ./var.env up -d && docker ps
