1971006a2df5b1bb97d69ec25e0767692f45e83d
Yay589 <yacine.said@insa-cvl.fr>
2025-01-11 20:33:09 +0100

    It's working

--- Client/dvwa-with-ssh/setup_ssh.sh
+++ Client/dvwa-with-ssh/setup_ssh.sh
@@ -6,20 +6,20 @@ set -x  # Affiche chaque commande exécutée
 
 
 # Install OpenSSH server  |  Python Packages  should be installed to make the ansible work  properly 
-#apt-get update -y && apt-get install -y openssh-server && apt install -y python3
+apt-get install -y openssh-server && apt install -y python3
 
 # Create directory for sshd to run
-mkdir /var/run/sshd
+#mkdir /var/run/sshd  (IF OPENSSH IS INSTALL THE REPO ALREADY EXIST !!!!)
 
 # Set root password
 echo "root:root" | chpasswd
 
 # Enable root login and password authentication in sshd_config
-#sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-#sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
 
 # Open the ssh  port 22
-#sed -i 's/^#Port 22/Port 22/' /etc/ssh/sshd_config
+sed -i 's/^#Port 22/Port 22/' /etc/ssh/sshd_config
 
 #apache
 grep -q '^ServerName' /etc/apache2/apache2.conf || echo 'ServerName 8.8.8.8' |  tee -a /etc/apache2/apache2.conf
@@ -27,4 +27,4 @@ grep -q '^ServerName' /etc/apache2/apache2.conf || echo 'ServerName 8.8.8.8' |  
 apachectl -D FOREGROUND &
 
 # Start SSH service
-#service ssh start
+service ssh start
